75f9b5974c9e0b2a59c574b9fd912f4c615fd116
RishabCSS <gtavrishab@gmail.com>
2025-04-09 00:32:26 +0530

    1

--- server.js
+++ server.js
@@ -10,5 +10,6 @@ app.listen(PORT, () => {
   console.log(`Web server running on port ${PORT}`);
 
   // Start the Discord bot after the web server is ready
-  require("./index.js");
 });
+
+  require("./index.js");
